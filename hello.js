bffd9021e785b8078e674caa9f809d24589490c5
JavisStark <97118498+JavisStark@users.noreply.github.com>
2022-10-28 13:07:37 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hola mundo desde México, curso de Cloud y Devops, tarea 4.2 JavisStark otra oportunidad 2";
+const hello = () => "Hola mundo desde México, curso de Cloud y Devops, tarea 4.2 JavisStark otra oportunidad 3";
 console.log(hello());
 exports.hello = hello;
